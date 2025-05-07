cbb3ad2245c3fb54b050af3e56ce6f27aa982ba8
mohamed3zz <elgendymohamed7y@gmail.com>
2025-05-07 12:10:33 +0300

    first commit

---
+++ course1/main.js
@@ -0,0 +1,42 @@
+// let nav = document.querySelectorAll(".sec3 .navbar ul li");
+// let sections = document.querySelectorAll("section")
+// let sec1 = document.querySelector(".section1")
+// let menu = document.querySelectorAll("ul li a")
+// console.log(menu);
+// totop.onclick = () => {
+// scrollTo({top:0,
+// behavior:"smooth"
+// })
+// }
+
+// window.addEventListener("scroll" , () => { 
+
+// let winscroll = window.scrollY;
+// if (winscroll>sections[1].offsetTop)
+// {
+// totop.classList.add(`toup`)
+// }
+// else {
+// totop.classList.remove(`toup`)
+// }
+// sections.forEach((section) => {
+// let sectionid =section.getAttribute("id");
+// let sectionoffsettop = section.offsetTop;
+
+
+// menu.forEach((menu) => {
+// let sectionref=menu.getAttribute("href").slice(1);
+
+// if (winscroll>=sectionoffsettop - 200) {
+//     if (sectionid==sectionref) {
+//         menu.classList.add(`active`);
+//         document.title=`BOM | ${sectionid}`;
+//     }
+//     else {
+//         menu.classList.remove(`active`);
+//     }
+// }
+// })
+// })
+
+// })
